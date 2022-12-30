3c420e829ff34815196dc79d4d078126618323f0
demtro <77480264+demtro@users.noreply.github.com>
2022-12-30 07:14:19 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img class="m-auto" alt="Vue logo" src="./assets/demtrodev-logo.png">
+  <HelloWorld class="container" msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;700&display=swap');
+
+*{
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  
+  font-family: 'Plus Jakarta Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
